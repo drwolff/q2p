a7ba2a4d87c888bf6ebf05f579c0541942cdaff6
drwolff <drwolff@hawaii.edu>
2015-03-28 13:19:28 -0500

    Update q2p.py

--- q2p.py
+++ q2p.py
@@ -1,20 +1,18 @@
 from nltk import tokenize
 import nltk.data
 from wand.image import Image
-from wand.drawing import Drawing
+from wand.font import Font
 
 n = 2
 f = open('tesla.txt').read()
 sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
-#print ('\n \n'.join(sent_detector.tokenize(f.strip())))
+
 for x in sent_detector.tokenize(f.strip()):
     with Image(filename='1.jpg') as img:
-        with Drawing() as draw:
-            draw.font = 'C:\Windows\Fonts\BRUSHSCI.TTF'
-            draw.font_size = 100
-            draw.text(100, 150, x)
-            draw(img)
+        font=Font(
+            path=('C:\Windows\Fonts\BRUSHSCI.TTF'),
+            size=60)
+        img.caption(x,100,50,1300,500,font,gravity='west')
         temp = str(n)+ ('.jpg')
         img.save(filename=temp)
         n += 1
-        
